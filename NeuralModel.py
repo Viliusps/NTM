0f0acac3a16947dc83195dac459e9428d41e6cea
simsve3 <simsve3@ktu.lt>
2023-05-23 23:25:35 +0300

    Added new data and minor changes

--- NeuralModel.py
+++ NeuralModel.py
@@ -5,20 +5,29 @@ from keras.layers import Input, Dense
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
 
 data_path = './data/cars.csv'
+data_path1 = './data/cars1.csv'
 image_dir = './data/images/'
-df = pd.read_csv(data_path)
+df = pd.concat(map(pd.read_csv,[data_path,data_path1]))
 
-selected_columns = ['price', 'gearbox', 'kilometrage', 'make_date', 'fuel_id']
-df = df.loc[:, selected_columns]
-df['price'] = df['price'].str.replace(' ', '')
-df['price'] = df['price'].astype(float)
+selected_columns = ['price', 'kilometrage', 'make_date', 'engine']
 
-for col in df.columns:
-    if df[col].dtype == 'object':
-        label_encoder = LabelEncoder()
-        df[col] = label_encoder.fit_transform(df[col].astype(str))
+df = df[selected_columns]
+df['price'] = df['price'].str.replace(' ', '').astype(float)
+df['kilometrage'] = df['kilometrage'].str.replace(' ', '').astype(float)
+df['make_date'] = df['make_date'].str.split('-').str[0].astype(int)
+df['engine'] = df['engine'].str.extract(r'AG \((\d+)kW\)').astype(float)
+
+#Convert specific columns to numeric
+columns_to_encode = []#['gearbox', 'fuel_id']
+
+#Encode categorical columns
+label_encoder = LabelEncoder()
+df[columns_to_encode] = df[columns_to_encode].apply(lambda x: label_encoder.fit_transform(x))
 
 df.dropna(inplace=True)
 
@@ -26,18 +35,24 @@ num_features = len(selected_columns) - 1
 
 input_features = Input(shape=(num_features,))
 dense1 = Dense(64, activation='relu')(input_features)
-
 dense2 = Dense(128, activation='relu')(dense1)
-output = Dense(1, activation='linear')(dense2)
+dense3 = Dense(128, activation='relu')(dense2)
+dense4 = Dense(128, activation='relu')(dense3)
+dense5 = Dense(128, activation='relu')(dense4)
+output = Dense(1, activation='linear')(dense5)
+
+print(df.head())
+sns.heatmap(df.corr(), annot=True)
+plt.show()
 
 model = Model(inputs=input_features, outputs=output)
 
-model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['MeanAbsoluteError'])
 
 model.summary()
 
 numeric_data = df.loc[:, selected_columns[1:]].copy()
 numeric_data = numeric_data.values.astype(np.float32)
-print(numeric_data)
+numeric_data = np.reshape(numeric_data, (numeric_data.shape[0], num_features))
 
-model.fit(numeric_data, df['price'], epochs=500, batch_size=50, verbose=2)
+model.fit(numeric_data, df['price'], epochs=1000, batch_size=20, verbose=1)
--- NeuralModelImages.py
+++ NeuralModelImages.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from keras.callbacks import CSVLogger
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, concatenate
 from keras.optimizers import Adam
@@ -7,16 +8,23 @@ from keras.preprocessing.image import ImageDataGenerator
 from sklearn.preprocessing import LabelEncoder
 
 data_path = './data/cars.csv'
+data_path1 = './data/cars1.csv'
 image_dir = './data/images/'
-df = pd.read_csv(data_path)
+df = pd.concat(map(pd.read_csv,[data_path,data_path1]))
 
-selected_columns = ['title', 'price', 'gearbox', 'image', 'kilometrage', 'make_date', 'fuel_id']
-df = df.loc[:, selected_columns]
+selected_columns = ['price', 'image', 'kilometrage', 'make_date', 'engine']
+df = df[selected_columns]
+df['price'] = df['price'].str.replace(' ', '').astype(float)
+df['kilometrage'] = df['kilometrage'].str.replace(' ', '').astype(float)
+df['make_date'] = df['make_date'].str.split('-').str[0].astype(int)
+df['engine'] = df['engine'].str.extract(r'AG \((\d+)kW\)').astype(float)
 
-for col in df.columns:
-    if df[col].dtype == 'object' and col not in ['image', 'title']:
-        label_encoder = LabelEncoder()
-        df[col] = label_encoder.fit_transform(df[col].astype(str))
+#Convert specific columns to numeric
+columns_to_encode = []#['gearbox', 'fuel_id']
+
+#Encode categorical columns
+label_encoder = LabelEncoder()
+df[columns_to_encode] = df[columns_to_encode].apply(lambda x: label_encoder.fit_transform(x))
 
 df.dropna(inplace=True)
 
@@ -31,18 +39,18 @@ conv1 = Conv2D(32, (3, 3), activation='relu')(input_img)
 pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 flatten_img = Flatten()(pool1)
 
-
 input_features = Input(shape=(num_features,))
 dense1 = Dense(64, activation='relu')(input_features)
-
 merged = concatenate([flatten_img, dense1])
-
 dense2 = Dense(128, activation='relu')(merged)
-output = Dense(1, activation='linear')(dense2)
+dense3 = Dense(128, activation='relu')(dense2)
+dense4 = Dense(128, activation='relu')(dense3)
+dense5 = Dense(128, activation='relu')(dense4)
+output = Dense(1, activation='linear')(dense5)
 
 model = Model(inputs=[input_img, input_features], outputs=output)
 
-model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['MeanAbsoluteError'])
 
 model.summary()
 
@@ -54,7 +62,7 @@ image_data_generator = datagen.flow_from_dataframe(
     x_col='image',
     y_col='price',
     target_size=(image_height, image_width),
-    batch_size=1676,
+    batch_size=len(df),
     class_mode='raw'
 )
 
@@ -62,5 +70,6 @@ numeric_data = df.loc[:, selected_columns[1:]].copy()
 numeric_data.drop('image', axis=1, inplace=True)
 numeric_data = numeric_data.values.astype(np.float32)
 
-model.fit([image_data_generator.next()[0], numeric_data], df['price'], epochs=500, batch_size=50, verbose=1)
+csv_logger = CSVLogger("epochs_logs.txt", separator='\t', append=True)
+model.fit([image_data_generator.next()[0], numeric_data], df['price'], epochs=1000, batch_size=30, verbose=1, callbacks=[csv_logger])
 
