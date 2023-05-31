5cf2d6531866216e828726f787a9d9aae7a19b74
Vilius <viliusps@gmail.com>
2023-05-31 16:47:57 +0300

    trained model

--- NeuralModelImages.py
+++ NeuralModelImages.py
@@ -6,53 +6,67 @@ from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, concatenat
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import keras
+from keras.layers import Dropout
+from sklearn.preprocessing import MinMaxScaler
 
 data_path = './data/cars.csv'
 data_path1 = './data/cars1.csv'
+data_path2 = './data/cars2.csv'
 image_dir = './data/images/'
-df = pd.concat(map(pd.read_csv,[data_path,data_path1]))
+df = pd.concat(map(pd.read_csv, [data_path, data_path1, data_path2]))
 
-selected_columns = ['price', 'image', 'kilometrage', 'make_date', 'engine']
+selected_columns = ['price','image', 'kilometrage', 'make_date', 'engine']
+x_columns = ['kilometrage', 'make_date', 'engine']
 df = df[selected_columns]
 df['price'] = df['price'].str.replace(' ', '').astype(float)
 df['kilometrage'] = df['kilometrage'].str.replace(' ', '').astype(float)
 df['make_date'] = df['make_date'].str.split('-').str[0].astype(int)
 df['engine'] = df['engine'].str.extract(r'AG \((\d+)kW\)').astype(float)
-
-#Convert specific columns to numeric
-columns_to_encode = []#['gearbox', 'fuel_id']
-
-#Encode categorical columns
-label_encoder = LabelEncoder()
-df[columns_to_encode] = df[columns_to_encode].apply(lambda x: label_encoder.fit_transform(x))
-
 df.dropna(inplace=True)
+X = df[x_columns]
+y = df['price']
 
-num_features = len(selected_columns) - 2
+# Check for outliers
+Q1 = X.quantile(0.25)
+Q3 = X.quantile(0.75)
+IQR = Q3 - Q1
+outliers = ((X < (Q1 - 1.5 * IQR)) | (X > (Q3 + 1.5 * IQR))).any(axis=1)
+df = df[~outliers]
+
+# Normalize the input features
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
 
 image_height = 224
 image_width = 224
 image_channels = 3
 
-input_img = Input(shape=(image_height, image_width, image_channels))
-conv1 = Conv2D(32, (3, 3), activation='relu')(input_img)
-pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-flatten_img = Flatten()(pool1)
+modelImages = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_height,image_width,image_channels)),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Conv2D(64, (3,3),activation='relu'),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Conv2D(64, (3,3),activation='relu'),
+    keras.layers.Flatten()
+])
 
-input_features = Input(shape=(num_features,))
-dense1 = Dense(64, activation='relu')(input_features)
-merged = concatenate([flatten_img, dense1])
-dense2 = Dense(128, activation='relu')(merged)
-dense3 = Dense(128, activation='relu')(dense2)
-dense4 = Dense(128, activation='relu')(dense3)
-dense5 = Dense(128, activation='relu')(dense4)
-output = Dense(1, activation='linear')(dense5)
+# Create the Keras model
+model = keras.Sequential([
+    keras.layers.Dense(128, input_dim=3, activation='relu'),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dense(128, activation='relu'),
+])
 
-model = Model(inputs=[input_img, input_features], outputs=output)
+merged = concatenate([modelImages.output, model.output])
 
-model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['MeanAbsoluteError'])
+output = keras.layers.Dense(1, activation='linear')(merged)
+combined_model = keras.Model(inputs=[modelImages.input, model.input], outputs=output)
 
-model.summary()
+combined_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['MeanAbsoluteError'])
+
+combined_model.summary()
 
 datagen = ImageDataGenerator(rescale=1.0/255.0)
 
@@ -71,6 +85,6 @@ numeric_data.drop('image', axis=1, inplace=True)
 numeric_data = numeric_data.values.astype(np.float32)
 
 csv_logger = CSVLogger("epochs_logs.txt", separator='\t', append=True)
-model.fit([image_data_generator.next()[0], numeric_data], df['price'], epochs=250, batch_size=30, verbose=1, callbacks=[csv_logger])
+combined_model.fit([image_data_generator.next()[0], numeric_data], df['price'], epochs=250, batch_size=30, verbose=2, callbacks=[csv_logger])
 
-model.save('model')
+combined_model.save('model')
